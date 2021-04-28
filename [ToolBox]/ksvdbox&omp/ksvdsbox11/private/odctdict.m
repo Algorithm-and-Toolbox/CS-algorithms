814698025b594720d8b33835169df7fc2204e34e
Zhihong <z_zhi_hong@163.com>
2021-04-28 10:35:22 +0800

    update odctdict

--- [ToolBox]/ksvdbox&omp/ksvdsbox11/private/odctdict.m
+++ [ToolBox]/ksvdbox&omp/ksvdsbox11/private/odctdict.m
@@ -13,10 +13,22 @@ function D = odctdict(n,L)
 %  April 2009
 
 
+%% original implementation by ronrubin
+% D = zeros(n,L);
+% D(:,1) = 1/sqrt(n);
+% for k = 2:L
+%   v = cos((0:n-1)*pi*(k-1)/L)';
+%   v = v-mean(v);
+%   D(:,k) = v/norm(v);
+% end
+
+%% accelerated by Zhihong Zhang
 D = zeros(n,L);
 D(:,1) = 1/sqrt(n);
-for k = 2:L
-  v = cos((0:n-1)*pi*(k-1)/L)';
-  v = v-mean(v);
-  D(:,k) = v/norm(v);
-end
+
+[X,Y] = ndgrid(0:n-1,2:L);
+V = cos(X.*pi.*(Y-1)./L);
+V = V-mean(V);
+V = V./repmat(vecnorm(V),n,1);
+
+D(:,2:end) = V;
